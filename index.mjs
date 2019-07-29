b715d726ee084e37d93bdebe080a08b0ca88f084
Taylor Blanton <matthewtblanton@gmail.com>
2019-07-28 22:33:44 -0500

    it's working

--- index.mjs
+++ index.mjs
@@ -1,7 +1,12 @@
-// import discord from 'discord.js';
+import discord from 'discord.js';
 import resources from './resources';
-// const client = new discord.Client();
+import { readFileSync } from 'fs';
+import { stringify } from 'querystring';
 
+/**
+ * @param {[string, string]} titleValueArray array of titles and values
+ * @returns {{title: string, value: string}} Object with title and space with matching length
+ */
 const getEntriesWithSpaces = ([title, value]) => {
     const titleSpaces = ' '.repeat(Math.max(0, value.length - title.length));
     const valueSpaces = ' '.repeat(Math.max(0, title.length - value.length));
@@ -15,6 +20,11 @@ const getEntriesWithSpaces = ([title, value]) => {
     }
 };
 
+/**
+ * @param {string} character
+ * @param {string} move
+ * @param {[string, string][]} values
+ */
 const getMessageToSend = (character, move, values) => {
     const entriesWithSpaces = values.map(getEntriesWithSpaces);
     const tableHeaders = entriesWithSpaces.reduce((acc, current) => acc += current.title + ` | `, '');
@@ -29,6 +39,7 @@ const getMessageToSend = (character, move, values) => {
 }
 
 const getValuesMap = moveData => {
+    console.log(moveData);
     const valuesMap = new Map();
 
     valuesMap.set('Startup', moveData['Active On']);
@@ -41,26 +52,49 @@ const getValuesMap = moveData => {
 }
 
 // !uf <character> <move>
-const handleMessage = async msg => {
+
+/**
+ * @param {import('discord.js').Message} msg
+ */
+const handleMessage = msg => {
     if (msg.content.startsWith('!uf')) {
         const splitContent = msg.content.trim().split(' ');
         const character = splitContent[1].toLowerCase();
-        const move = splitContent[2].toUpperCase();
+        let move = splitContent[2];
+
+        if(move.startsWith('j.')) {
+            const splitMove = move.split('j.');
+            move = 'j.' + splitMove[1].toUpperCase();
+        } else {
+            move = move.toUpperCase();
+        }
 
         const characterData = resources[character];
+        if (!characterData) {
+            msg.channel.send(`Couldn't find character ${character}!`)
+            return;
+        }
 
         const moveData = characterData[move];
+        if(!moveData) {
+            msg.channel.send(`Couldn't find ${character}'s move ${move}`);
+            return;
+        }
 
         const valuesMap = getValuesMap(moveData);
-
         const messageToSend = getMessageToSend(character, move, Array.from(valuesMap));
 
-        console.log(messageToSend);
+        console.log(`Sending:\n${messageToSend}`);
+        msg.channel.send(messageToSend);
     }
 }
 
-// client.on('message', handleMessage)
+const client = new discord.Client();
 
-const args = process.argv.slice(2);
+client.on('ready', () => {
+    console.log('Ready!');
+});
 
-handleMessage({content: args[0]});
+client.on('message', handleMessage);
+
+client.login(readFileSync('./token').toString())
